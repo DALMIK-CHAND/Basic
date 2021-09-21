1f9a1f0f6c7437a1c62d8cde1539fcfdf81fdc79
DALMIK-CHAND <91128506+DALMIK-CHAND@users.noreply.github.com>
2021-09-21 22:02:21 +0530

    Add files via upload

---
+++ Basic2/index.php
@@ -0,0 +1,105 @@
+<!doctype html>
+    <html lang="en">
+      <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- font awesome-->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+       <!-- owl carousel -->
+       <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.css">
+       <!--fonts   -->
+      <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
+       
+       <link rel="stylesheet" href="style.css" type="text/css">
+        <title>Home</title>
+      </head>
+      <body>
+
+        <!--menu-->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand btn btn-outline-danger btn-lg" href="viewusers.php">BANKING SYSTEM</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarScroll">
+      <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+        <li class="nav-item">
+          <a class="btn btn-outline-warning btn-lg" aria-current="page" href="index.php">Home</a>
+        </li>
+         </div>
+      </div>
+</nav>
+
+
+<!-- carousel -->
+
+        <div id="demo" class="carousel slide" data-ride="carousel">
+          <!-- indicators -->
+          <ul class="carousel-indicators">
+              <li data-target="demo" data-slide-to="0" class="active"></li>
+              <li data-target="demo" data-slide-to="2"></li>
+              <li data-target="demo" data-slide-to="3"></li>
+          </ul>
+
+          <!-- slideshow -->
+          <div class="carousel-inner">
+
+            <div class="carousel-item active">
+              <img src="image6.jpg" alt="image 1" width="1300" height="700">
+              <div class="carousel-caption d-none d-md-block">
+                <h1>A.B.C CORPORATION BANK <br> <h5>IF YOU WANT TO TRANSFER AMOUNT CKICK ON TRANFER AMOUNT BUTTON <br> CLICK ON VIEW TRANSECTION TO SEE SUMMARY</h5></h1>
+                <p>Some representative placeholder content for the first slide.</p>
+                <a href="viewusers.php" class="btn btn-danger btn-lg" role="button">Transfer amount</a>
+                <a href="transactions.php" class="btn btn-danger btn-lg" role="button">View transactions</a>
+              </div>
+            </div>
+
+            <div class="carousel-item">
+              <img src="image2.jpg" alt="image 2" width="1300" height="700">
+              <div class="carousel-caption d-none d-md-block">
+                <h1>A.B.C CORPORATION BANK <br> <h5>IF YOU WANT TO TRANSFER AMOUNT CKICK ON TRANFER AMOUNT BUTTON <br> CLICK ON VIEW TRANSECTION TO SEE SUMMARY</h5></h1>
+                <p>Some representative placeholder content for the first slide.</p>
+                <a href="viewusers.php" class="btn btn-warning btn-lg" role="button">Transfer amount</a>
+                <a href="transactions.php" class="btn btn-warning btn-lg" role="button">View transactions</a>
+              </div>
+            </div>
+
+            <div class="carousel-item">
+              <img src="image5.jpg" alt="image 3" width="1300" height="700">
+              <div class="carousel-caption d-none d-md-block">
+                <h1>A.B.C CORPORATION BANK <br> <h5>IF YOU WANT TO TRANSFER AMOUNT CKICK ON TRANFER AMOUNT BUTTON <br> CLICK ON VIEW TRANSECTION TO SEE SUMMARY</h5></h1>
+                
+                <a href="viewusers.php" class="btn btn-info btn-lg" role="button">Transfer amount</a>
+                <a href="transactions.php" class="btn btn-info btn-lg" role="button">View transactions</a>
+              </div>
+            </div>
+          </div>
+
+          <!-- controls -->
+
+          <a class="carousel-control-prev" href="#demo" data-slide="prev">
+            <span class="carousel-control-prec-icon"></span>
+          </a>
+          <a class="carousel-control-next" href="#demo" data-slide="next">
+            <span class="carousel-control-prec-icon"></span>
+          </a>
+        </div>
+
+    
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.js"></script>
+        <script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js"></script>        
+      </body>
+    </html>
+ 
